ccdd8b8ab3453ddb288ead94307159dfdef934dd
Fehlpe <felpearcanjo@gmail.com>
2023-02-23 19:37:42 -0300

    entity orm fix

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -1,12 +1,13 @@
 import "dotenv/config";
 import { DataSourceOptions } from "typeorm";
+import { UsuarioEntity } from "./entities/usuario.entity";
 
 const config: DataSourceOptions = {
   type: "postgres",
   url: process.env.DATABASE_URL,
   synchronize: false,
   logging: false,
-  entities: ["src/database/entities/**/*"],
+  entities: [UsuarioEntity],
   migrations: ["src/database/migrations/**/*.ts"],
   // ssl: {
   //   rejectUnauthorized: false,
