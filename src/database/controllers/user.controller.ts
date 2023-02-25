1430d66638c90600efe3b32bb3f9b808d8a6a95c
Fehlpe <felpearcanjo@gmail.com>
2023-02-25 17:03:56 -0300

    user controller req-body fix

--- src/database/controllers/user.controller.ts
+++ src/database/controllers/user.controller.ts
@@ -3,20 +3,20 @@ import { UserRepository } from "../repositories/user.repository";
 
 export class UserController {
   async create(req: Request, res: Response) {
-    const { nome, email, senha } = req.body;
+    const { username, email, password } = req.body;
 
     const repository = new UserRepository();
 
-    const user = await repository.createUser(nome, email, senha);
+    const user = await repository.createUser(username, email, password);
 
     return res.json(user);
   }
 
   async login(req: Request, res: Response) {
-    const { email, senha } = req.body;
+    const { email, password } = req.body;
 
     const repository = new UserRepository();
-    const user = await repository.checkUserLogin(email, senha);
+    const user = await repository.checkUserLogin(email, password);
 
     if (!user) {
       return res.status(401).json({ error: "Incorrect username or password" });
